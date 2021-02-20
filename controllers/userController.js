685fbcd0e7ed78b591998fb723b43f35283a80fb
Nayeem-17 <nasa.28apr@gmail.com>
2021-02-21 00:37:14 +0600

    Allah e jane ken kortechi eshob T_T

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,39 +38,39 @@ module.exports.userReg = async (req, res) => {
 }
 
 module.exports.getQuestions = async (req, res) => {
-    // try {
-    const userId = req.body.userId;
-    const examId = req.params.examId;
-    // updating stat
-    let questionSet = await QuestionSetModel.find({ uuid: examId });
-    // console.log(questionSet)
-    questionSet[0].studentId.push(userId);
-    await QuestionSetModel.findOneAndUpdate({ uuid: examId }, questionSet[0], {
-        new: true,
-        useFindAndModify: false
+    try {
+        const userId = req.body.userId;
+        const examId = req.params.examId;
+        // updating stat
+        let questionSet = await QuestionSetModel.find({ uuid: examId });
+        // console.log(questionSet)
+        questionSet[0].studentId.push(userId);
+        await QuestionSetModel.findOneAndUpdate({ uuid: examId }, questionSet[0], {
+            new: true,
+            useFindAndModify: false
 
-    });
-    // updating dashboard
-    let userDashBoard = await dashboardModel.find({ userId: userId });
-    console.log(userDashBoard);
-    userDashBoard = userDashBoard[0];
-    userDashBoard.exams.push({
-        questionId: examId,
-        marks: 0
-    });
-    await dashboardModel.findOneAndUpdate({ userId: userId }, userDashBoard, {
-        new: true,
-        useFindAndModify: false
+        });
+        // updating dashboard
+        let userDashBoard = await dashboardModel.find({ userId: userId });
+        console.log(userDashBoard);
+        userDashBoard = userDashBoard[0];
+        userDashBoard.exams.push({
+            questionId: examId,
+            marks: 0
+        });
+        await dashboardModel.findOneAndUpdate({ userId: userId }, userDashBoard, {
+            new: true,
+            useFindAndModify: false
 
-    });
+        });
 
-    res.json({
-        questions: questionSet[0].questions
-    });
+        res.json({
+            questions: questionSet[0].questions
+        });
 
-    // } catch (error) {
-    //     res.status(403).json({ error: error })
-    // }
+    } catch (error) {
+        res.status(403).json({ error: error })
+    }
 }
 
 module.exports.submitAnswers = async (req, res) => {
